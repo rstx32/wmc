609dc64e244f6b4f023b0d23820c643f3c30f3f9
rstx <rstx32@gmail.com>
2020-05-17 05:38:25 +0700

    clean code

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,30 +12,25 @@ use Carbon\Carbon;
 
 class AdminController extends Controller
 {
-    public function index()
-    {
+    public function index()    {
         $user = DB::table('users')->where('admin',0)->orderByDesc('skor')->get();
         $soal = DB::table('bank_soal')->get();
         $status = DB::table('sesi')->where('id',10)->get();
         $carbon = Carbon::now('Asia/Jakarta'); 
-            
     	return view('admin.index')->with(compact('user','soal','status','carbon'));
     }
 
-    public function create()
-    {
+    public function create()    {
         $soal = DB::table('bank_soal')->get()->count();
         return view('admin.tambahSoal');
     }
 
-    public function edit($id)
-    {
+    public function edit($id)    {
         $soal = DB::table('bank_soal')->where('id',$id)->get();
         return view('admin.editSoal',['soal' => $soal]);
     }
 
-    public function update(Request $request)
-    {
+    public function update(Request $request)    {
         DB::table('bank_soal')->where('id',$request->id)->update([
             'soal' => $request->soal,
             'jawaban' => $request->jawaban,
@@ -56,6 +51,7 @@ class AdminController extends Controller
             'jawaban' => $request->jawaban,
             'poin' => $request->poin,
         ]);
+
     	return redirect('/admin#soal');
     }
 
@@ -66,7 +62,7 @@ class AdminController extends Controller
 
     public function exportPDF(){
         $score = DB::table('users')->where('admin',0)->orderByDesc('skor')->get();
-		$pdf = PDF::loadview('admin.score_pdf',['score'=>$score]);
+        $pdf = PDF::loadview('admin.score_pdf',['score'=>$score]);
 		return $pdf->download('live-score-pdf');
     }
 
@@ -76,9 +72,7 @@ class AdminController extends Controller
             'wkt_selesai' => Carbon::now('Asia/Jakarta')->addMinutes($request->durasi),
             'status' => 1
         ]);
-
         DB::table('users')->update(['skor' => 0]);
-
         return redirect('/admin');
     }
 
--- app/Http/Controllers/PesertaController.php
+++ app/Http/Controllers/PesertaController.php
@@ -11,52 +11,50 @@ use Illuminate\Support\Facades\DB;
 
 class PesertaController extends Controller
 {
-    public function __construct(){
-        $this->middleware('auth');
-    }
+	public function __construct(){
+		$this->middleware('auth');
+	}
 
-    public function index(){
-      $user = DB::table('users')->where('admin',0)->orderByDesc('skor')->get();
-      $status = DB::table('sesi')->where('id',10)->get();
+	public function index(){
+		$user = DB::table('users')->where('admin',0)->orderByDesc('skor')->get();
+		$status = DB::table('sesi')->where('id',10)->get();
 
-      foreach($status as $s){
-        if($s->status==1){
-          $terjawab = DB::table('terjawab')->where('peserta',Auth::id())->get();
-          if($terjawab->count()<1){
-            $soal = DB::table('bank_soal')->get();
-          }else{
-            $x = [];
-            foreach ($terjawab as $t){
-              array_push($x,$t->soal);
-            }
-            $soal = DB::table('bank_soal')->whereNotIn('id',$x)->get();
-          }
-          $waktu = $s->wkt_selesai;
-          return view('peserta.index')->with(compact('user', 'terjawab', 'soal', 'waktu'));
-        }
-        else
-          return view('peserta.disable')->with(compact('user'));
-      }      
-    }    
+		foreach($status as $s){
+			if($s->status==1){
+				$terjawab = DB::table('terjawab')->where('peserta',Auth::id())->get();
+				if($terjawab->count()<1){
+					$soal = DB::table('bank_soal')->get();
+				}else{
+					$x = [];
+					foreach ($terjawab as $t){
+						array_push($x,$t->soal);
+					}
+					$soal = DB::table('bank_soal')->whereNotIn('id',$x)->get();
+				}
+				$waktu = $s->wkt_selesai;
+				return view('peserta.index')->with(compact('user', 'terjawab', 'soal', 'waktu'));
+			}else
+				return view('peserta.disable')->with(compact('user'));
+		}      
+	}    
 
-    public function submit($id, Request $request){
-    	$this->validate($request,['jawaban' => 'required']);
+	public function submit($id, Request $request){
+		$this->validate($request,['jawaban' => 'required']);
 
-      $jawabanBenar = DB::table('bank_soal')->where('id',$id)->first();
-      $jawabanInput = $request->jawaban;
+		$jawabanBenar = DB::table('bank_soal')->where('id',$id)->first();
+		$jawabanInput = $request->jawaban;
 
-      if(strcmp($jawabanBenar->jawaban,$jawabanInput)==0){
-        $skorSkrg = DB::table('users')->where('id',Auth::id())->first();
-        $update = $skorSkrg->skor + $jawabanBenar->poin;
-        DB::table('users')->where('id',Auth::id())->update(['skor' => $update]);
-        DB::table('terjawab')->insert([
-          'soal' => $jawabanBenar->id,
-          'peserta' => Auth::id(),
-        ]);
-        
-        return redirect('/peserta');
-      }else{
-        return view('peserta.salah', ['benar' => $jawabanBenar->jawaban], ['input' => $jawabanInput]);
-      }
-    }
+		if(strcmp($jawabanBenar->jawaban,$jawabanInput)==0){
+			$skorSkrg = DB::table('users')->where('id',Auth::id())->first();
+			$update = $skorSkrg->skor + $jawabanBenar->poin;
+			DB::table('users')->where('id',Auth::id())->update(['skor' => $update]);
+			DB::table('terjawab')->insert([
+				'soal' => $jawabanBenar->id,
+				'peserta' => Auth::id(),
+			]);
+			return redirect('/peserta');
+		}else{
+			return view('peserta.salah', ['benar' => $jawabanBenar->jawaban], ['input' => $jawabanInput]);
+		}
+	}
 }
